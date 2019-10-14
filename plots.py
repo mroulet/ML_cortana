7d676c4495fbf582514c2a8d5934f23ae05ae702
mroulet <margaux.roulet@epfl.ch>
2019-10-14 18:59:04 +0200

    Add files via upload

---
+++ plots.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""a function of ploting figures."""
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_fitted_curve(y, x, weights, degree, ax):
+    """plot the fitted curve."""
+    ax.scatter(x, y, color='b', s=12, facecolors='none', edgecolors='r')
+    xvals = np.arange(min(x) - 0.1, max(x) + 0.1, 0.1)
+    tx = build_poly(xvals, degree)
+    f = tx.dot(weights)
+    ax.plot(xvals, f)
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_title("Polynomial degree " + str(degree))
+
+def plot_train_test(train_errors, test_errors, lambdas, degree):
+    """
+    train_errors, test_errors and lambas should be list (of the same size) the respective train error and test error for a given lambda,
+    * lambda[0] = 1
+    * train_errors[0] = RMSE of a ridge regression on the train set
+    * test_errors[0] = RMSE of the parameter found by ridge regression applied on the test set
+    
+    degree is just used for the title of the plot.
+    """
+    plt.semilogx(lambdas, train_errors, color='b', marker='*', label="Train error")
+    plt.semilogx(lambdas, test_errors, color='r', marker='*', label="Test error")
+    plt.xlabel("lambda")
+    plt.ylabel("RMSE")
+    plt.title("Ridge regression for polynomial degree " + str(degree))
+    leg = plt.legend(loc=1, shadow=True)
+    leg.draw_frame(False)
+    plt.savefig("ridge_regression")
+
+
+def cross_validation_visualization(lambds, mse_tr, mse_te):
+    """visualization the curves of mse_tr and mse_te."""
+    plt.semilogx(lambds, mse_tr, marker=".", color='b', label='train error')
+    plt.semilogx(lambds, mse_te, marker=".", color='r', label='test error')
+    plt.xlabel("lambda")
+    plt.ylabel("rmse")
+    plt.title("cross validation")
+    plt.legend(loc=2)
+    plt.grid(True)
+    plt.savefig("cross_validation")
+
+
+def bias_variance_decomposition_visualization(degrees, rmse_tr, rmse_te):
+    """visualize the bias variance decomposition."""
+    rmse_tr_mean = np.expand_dims(np.mean(rmse_tr, axis=0), axis=0)
+    rmse_te_mean = np.expand_dims(np.mean(rmse_te, axis=0), axis=0)
+    plt.plot(
+        degrees,
+        rmse_tr.T,
+        'b',
+        linestyle="-",
+        color=([0.7, 0.7, 1]),
+        label='train',
+        linewidth=0.3)
+    plt.plot(
+        degrees,
+        rmse_te.T,
+        'r',
+        linestyle="-",
+        color=[1, 0.7, 0.7],
+        label='test',
+        linewidth=0.3)
+    plt.plot(
+        degrees,
+        rmse_tr_mean.T,
+        'b',
+        linestyle="-",
+        label='train',
+        linewidth=3)
+    plt.plot(
+        degrees,
+        rmse_te_mean.T,
+        'r',
+        linestyle="-",
+        label='test',
+        linewidth=3)
+    plt.ylim(0.2, 0.7)
+    plt.xlabel("degree")
+    plt.ylabel("error")
+    plt.title("Bias-Variance Decomposition")
+    plt.savefig("bias_variance")
